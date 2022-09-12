655849d4dfe448728df333b8681d673687d1d9ab
Fedora <bianca.stan@arnia.ro>
2022-09-13 00:21:17 +0300

    Update Square stories

--- src/stories/Square.stories.tsx
+++ src/stories/Square.stories.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 import { Story } from '@storybook/react';
 import Square from '../components/Square';
 import { ISquare } from '../components/Square/Square';
-// import Mark from '../components/Mark';
-// import { XMarkOutline } from './Mark.stories';
-// import { IMark } from '../components/Mark/Mark';
+import { PlayerTypes } from '../App';
 
 export default {
   title: 'Square',
@@ -13,14 +11,24 @@ export default {
 
 const Template: Story<ISquare> = (args: any) => <Square {...args} />
 
-export const DefaulSquare = Template.bind({});
+export const EmptySquareForPlayerX = Template.bind({});
+EmptySquareForPlayerX.args = {
+  player: PlayerTypes.PLAYER_X,
+}
 
-// export const SquareHoveredByX = Template.bind({});
-// SquareHoveredByX.args = {
-//   children: <Mark {...XMarkOutline.args as IMark} />
-// };
+export const FilledSquareForPlayerX = Template.bind({});
+FilledSquareForPlayerX.args = {
+  ...EmptySquareForPlayerX.args,
+  isFilled: true,
+}
 
-// export const SquareHoveredByO = Template.bind({});
-// SquareHoveredByO.args = {
-//   children: null
-// };
+export const EmptySquareForPlayerO = Template.bind({});
+EmptySquareForPlayerO.args = {
+  player: PlayerTypes.PLAYER_O,
+}
+
+export const FilledSquareForPlayerO = Template.bind({});
+FilledSquareForPlayerO.args = {
+  ...EmptySquareForPlayerO.args,
+  isFilled: true,
+}
