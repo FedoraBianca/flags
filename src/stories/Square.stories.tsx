e3f531a9787ed66ec5bd35bb9f062363cc33c0df
Fedora <bianca.stan@arnia.ro>
2022-09-14 16:12:02 +0300

    Update stories naming

--- src/stories/Square.stories.tsx
+++ src/stories/Square.stories.tsx
@@ -11,24 +11,24 @@ export default {
 
 const Template: Story<ISquare> = (args: any) => <Square {...args} />
 
-export const EmptySquareForPlayerX = Template.bind({});
-EmptySquareForPlayerX.args = {
+export const EmptyForPlayerX = Template.bind({});
+EmptyForPlayerX.args = {
   player: PlayerTypes.PLAYER_X,
 }
 
-export const FilledSquareForPlayerX = Template.bind({});
-FilledSquareForPlayerX.args = {
-  ...EmptySquareForPlayerX.args,
+export const FilledForPlayerX = Template.bind({});
+FilledForPlayerX.args = {
+  ...EmptyForPlayerX.args,
   isFilled: true,
 }
 
-export const EmptySquareForPlayerO = Template.bind({});
-EmptySquareForPlayerO.args = {
+export const EmptyForPlayerO = Template.bind({});
+EmptyForPlayerO.args = {
   player: PlayerTypes.PLAYER_O,
 }
 
-export const FilledSquareForPlayerO = Template.bind({});
-FilledSquareForPlayerO.args = {
-  ...EmptySquareForPlayerO.args,
+export const FilledForPlayerO = Template.bind({});
+FilledForPlayerO.args = {
+  ...EmptyForPlayerO.args,
   isFilled: true,
 }
